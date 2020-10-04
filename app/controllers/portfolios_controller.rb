ac9b644b37c034eec6995149d54ead1b5c6dc729
takuya-kinoshita20160224 <takuyakinoshita20160224@gmail.com>
2020-10-04 16:29:20 +0900

    一覧表示機能実装

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,8 +1,24 @@
 class PortfoliosController < ApplicationController
   def index
+    @portfolios = Portfolio.all
   end
 
   def new
     @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+    if @portfolio.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:url, :text, :title, :language).merge(user_id: current_user.id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,9 +17,10 @@ ActiveRecord::Schema.define(version: 2020_10_02_070108) do
     t.string "url", null: false
     t.string "language", null: false
     t.text "text", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_portfolios_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -35,4 +36,5 @@ ActiveRecord::Schema.define(version: 2020_10_02_070108) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "portfolios", "users"
 end
