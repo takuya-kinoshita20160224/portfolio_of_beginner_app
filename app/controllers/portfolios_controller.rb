d877b9c0d091558b18c2abd67613f2ca481d2ad5
takuya-kinoshita20160224 <takuyakinoshita20160224@gmail.com>
2020-10-05 20:16:56 +0900

    詳細表示機能、削除機能、編集機能の実装

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portrolio, only: [:edit, :show]
   def index
     @portfolios = Portfolio.all
   end
@@ -13,11 +14,39 @@ class PortfoliosController < ApplicationController
     if @portfolio.save
       redirect_to root_path
     else
-      render :new
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    portfolio = Portfolio.find(params[:id])
+    if portfolio.update(portfolio_params)
+      redirect_to root_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    portfolio = Portfolio.find(params[:id])
+    if portfolio.destroy
+      redirect_to root_path
+    else
+      render 'edit'
     end
   end
 
   private
+
+  def set_portrolio
+    @portfolio = Portfolio.find(params[:id])
+  end
 
   def portfolio_params
     params.require(:portfolio).permit(:url, :text, :title, :language).merge(user_id: current_user.id)
